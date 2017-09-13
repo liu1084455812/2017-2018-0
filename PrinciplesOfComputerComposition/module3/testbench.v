040bcad217e430ac629bf3168a1ba76d11baf872
liu1084455812 <liu108445581297@gmail.com>
2017-09-13 09:34:00 +0800

    添加引脚约数，修改testbench

--- PrinciplesOfComputerComposition/module3/testbench.v
+++ PrinciplesOfComputerComposition/module3/testbench.v
@@ -1,8 +1,5 @@
 `timescale 1ns/1ns  
-module testbench ( );
-    reg clk;
-    reg rst;
-    
+module testbench (input clk,input rst,input[4:0] RegAddr,input[1:0] ByteSel,output reg[7:0] Led);
     
     mips my_mips (clk,rst);
     
@@ -16,6 +13,18 @@ module testbench ( );
        
     always
        #50 clk = ~clk ;
-       
+     
+	always @ (posedge clk)
+	begin
+		if(RegAddr!=5'b00000)
+		begin
+		case(ByteSel)
+			2'b00:Led = my_mips.U_IM.im[RegAddr][7:0];
+			2'b01:Led = my_mips.U_IM.im[RegAddr][15:7];
+			2'b10:Led = my_mips.U_IM.im[RegAddr][23:16];
+			2'b11:Led = my_mips.U_IM.im[RegAddr][31:24];
+		endcase
+		end
+	end
 endmodule
            
